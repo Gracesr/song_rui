a34dface5ad19e4e2c56ad0be60474902b21f37b
LAPTOP-C9F17JB5\hp <1977502458@qq.com>
2018-10-23 23:14:39 +0800

    2018年10月   第8周 宋瑞

---
+++ 4.15/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int base=200;
+  dobule sales,salary;
+  while (sales =!-1 )
+   {
+     cout<<"Enter sales in dollars(-1 to end):$";
+     cin>>sales;
+     if(sales==-1)break;
+     salary=base+sales*0.09;
+     cout<<"Salary is:$"<<salary<<endl;
+     }
+        return 0;
+}
