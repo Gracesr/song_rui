6cbca18b3ae6ef630c67e8f48604b2d309269d89
LAPTOP-C9F17JB5\hp <1977502458@qq.com>
2018-10-27 21:40:13 +0800

    2018年  宋瑞   c++作业

---
+++ chap 04 ex_14/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main{}
+{
+    int account_number;
+    double a=0,b=0,c=0,d=0,e=0 ;
+
+    while{account_number!=-1}
+   {
+    cout<<"Enter account number{or -1 to quit}";
+    cin>>account;
+    if{account_number==-1}break;
+    cout<<"Enter beginning balance";
+    cin>>a;
+    cout<<"Enter total charges";
+    cin>>b;
+    cout<<"Enter total_credits";
+    cin>>c;
+    cout<<"Enter credit_limit";
+    cin>>d;
+    e=a+b-c;
+
+    if{e<=d}
+     {
+        cout<<"New balance is "<<e<<endl;
+     }
+        eles
+        {cout<<"New balance is"<<e<<endl;
+         cout<<"Account"<<account<<endl;
+         cout<<"Credit Limit"<<d<<endl;
+         cout<<"Balance"<<e<<endl;
+         cout << "Credit Limit Exceeded."<<endl;
+
+     }
+            return 0;
+ }
+
+
+
